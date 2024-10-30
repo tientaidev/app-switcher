e08d074e041b10daca0ec796f1a3c4c12ae2edf3
Tyler <tientaidev@gmail.com>
2024-10-30 22:37:06 +0700

    Update utils.ts
    Add cn helper

--- utils.ts
+++ utils.ts
@@ -1,8 +1,15 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 export const isValidUrl = (url: string) => {
   try {
-    new URL(url);
-    return true;
-  } catch(error) {
-    return false;
+    new URL(url)
+    return true
+  } catch (error) {
+    return false
   }
+}
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
